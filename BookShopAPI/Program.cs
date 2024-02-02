57ab95b0960450ec1ecce75c131a7bfeffd73e69
moham <moham@SANDBOX251-PC>
2024-02-03 01:32:33 +0300

    Before Migration

--- BookShopAPI/Program.cs
+++ BookShopAPI/Program.cs
@@ -1,3 +1,6 @@
+using CarRentalAPI.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<BookShopDbContext>(c => c.UseSqlServer(builder.Configuration.GetValue<string>("sqlconnect")));
 
 var app = builder.Build();
 
